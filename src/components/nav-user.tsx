cecaa57ebc0ffdd935097bfe088aa23d04ec034a
Matt Jenkinson <75292329+mattdjenkinson@users.noreply.github.com>
2025-12-29 15:48:09 +0000

    fix google image

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -86,7 +86,11 @@ export function NavUser({
               )}
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user?.image || ""} alt={user?.name || ""} />
+                <AvatarImage 
+                  src={user?.image || ""} 
+                  alt={user?.name || ""} 
+                  referrerPolicy="no-referrer"
+                />
                 <AvatarFallback className="rounded-lg">
                   {initials}
                 </AvatarFallback>
@@ -109,7 +113,11 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user?.image || ""} alt={user?.name || ""} />
+                  <AvatarImage 
+                    src={user?.image || ""} 
+                    alt={user?.name || ""} 
+                    referrerPolicy="no-referrer"
+                  />
                   <AvatarFallback className="rounded-lg">
                     {initials}
                   </AvatarFallback>
